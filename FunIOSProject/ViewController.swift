1400bc629c079287eba37b8f0fb3dbc27d4ca2b1
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2018-10-17 13:05:47 -0600

    Initial Commit

---
+++ FunIOSProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunIOSProject
+//
+//  Created by Runyan, Jacob on 10/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
