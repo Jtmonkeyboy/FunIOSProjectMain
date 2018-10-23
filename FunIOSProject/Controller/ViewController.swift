fdbda31590b6726857e262938a7c39c81ab849f8
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2018-10-23 11:56:38 -0600

    App is almost done. I need to make it so that the random number does not pick the same number.

---
+++ FunIOSProject/Controller/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  FunIOSProject
+//
+//  Created by Runyan, Jacob on 10/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var complimentButton: UIButton!
+    @IBOutlet weak var iLoveYou: UILabel!
+    @IBOutlet weak var incredible: UILabel!
+    @IBOutlet weak var beautiful: UILabel!
+    @IBOutlet weak var emojiOK: UILabel!
+    
+    var randomNumber : Int = Int(arc4random() % 5)
+    
+    override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func complimentClick(_ sender: Any)-> Void
+    {
+        if(randomNumber == 1)
+        {
+            iLoveYou.isHidden = false
+            
+            incredible.isHidden = true
+            beautiful.isHidden = true
+            emojiOK.isHidden = true
+        }
+        if(randomNumber == 2)
+        {
+            incredible.isHidden = false
+            
+            iLoveYou.isHidden = true
+            beautiful.isHidden = true
+            emojiOK.isHidden = true
+        }
+        if(randomNumber == 3)
+        {
+            beautiful.isHidden = false
+            
+            iLoveYou.isHidden = true
+            incredible.isHidden = true
+            emojiOK.isHidden = true
+        }
+        if(randomNumber == 4)
+        {
+            emojiOK.isHidden = false
+            
+            iLoveYou.isHidden = true
+            incredible.isHidden = true
+            beautiful.isHidden = true
+        }
+        randomNumber = Int(arc4random() % 5)
+    }
+    
+}
+
---
+++ FunIOSProject/Model/Compliments.swift
@@ -0,0 +1,9 @@
+//
+//  Compliments.swift
+//  FunIOSProject
+//
+//  Created by Runyan, Jacob on 10/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
